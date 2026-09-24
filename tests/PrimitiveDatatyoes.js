ddd83df9c4e1e5224a586f931c2176ee33ab1c32
Karthik-123-rss <kumarkarthik452@gmail.com>
2026-09-24 14:09:03 +0530

    Primitive data types

---
+++ tests/PrimitiveDatatyoes.js
@@ -0,0 +1,11 @@
+ let age = 30;
+    let name = "Karthik H";
+    let isTrainer = true;
+    let salary = null;
+    let address;
+
+    console.log(age);
+    console.log(name);
+    console.log(isTrainer);
+    console.log(salary);
+    console.log(address);
